9183e9f25d56120945350bd742733973df066583
celineby <91615306+celineby@users.noreply.github.com>
2021-10-29 15:19:05 +0200

    Add files via upload

---
+++ script/pic_hover_effect.js
@@ -0,0 +1,51 @@
+const massPic1= document.getElementById("massPic1")
+
+function setNewImage(){
+    massPic1.src ="MassPic.jpeg";
+}
+
+function setOldImage(){
+    massPic1.src = "MassPic.png";
+}
+
+massPic1.addEventListener("mouseover", setNewImage);
+massPic1.addEventListener("mouseout", setOldImage);
+
+const massPic2= document.getElementById("massPic2")
+
+function setNewImage2(){
+    massPic2.src ="MassPic.jpeg";
+}
+
+function setOldImage2(){
+    massPic2.src = "MassPic.png";
+}
+
+massPic2.addEventListener("mouseover", setNewImage2);
+massPic2.addEventListener("mouseout", setOldImage2);
+
+const massPic3= document.getElementById("massPic3")
+
+function setNewImage3(){
+    massPic3.src ="MassPic.jpeg";
+}
+
+function setOldImage3(){
+    massPic3.src = "MassPic.png";
+}
+
+massPic3.addEventListener("mouseover", setNewImage3);
+massPic3.addEventListener("mouseout", setOldImage3);
+
+const massPic4= document.getElementById("massPic4")
+
+function setNewImage4(){
+    massPic4.src ="MassPic.jpeg";
+}
+
+function setOldImage4(){
+    massPic4.src = "MassPic.png";
+}
+
+massPic4.addEventListener("mouseover", setNewImage4);
+massPic4.addEventListener("mouseout", setOldImage4);
