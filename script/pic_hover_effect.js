c5d06ad9b1b879f337542bd5f5cbd9e281e20a21
celineby <91615306+celineby@users.noreply.github.com>
2021-11-01 16:29:00 +0100

    img change

--- script/pic_hover_effect.js
+++ script/pic_hover_effect.js
@@ -1,11 +1,11 @@
 const massPic1= document.getElementById("massPic1")
 
 function setNewImage(){
-    massPic1.src ="MassPic.jpeg";
+    massPic1.src ="img/daniel2.png";
 }
 
 function setOldImage(){
-    massPic1.src = "MassPic.png";
+    massPic1.src = "img/daniel1.png";
 }
 
 massPic1.addEventListener("mouseover", setNewImage);
@@ -14,11 +14,11 @@ massPic1.addEventListener("mouseout", setOldImage);
 const massPic2= document.getElementById("massPic2")
 
 function setNewImage2(){
-    massPic2.src ="MassPic.jpeg";
+    massPic2.src ="img/daniel1.png";
 }
 
 function setOldImage2(){
-    massPic2.src = "MassPic.png";
+    massPic2.src = "img/celine1.png";
 }
 
 massPic2.addEventListener("mouseover", setNewImage2);
@@ -27,11 +27,11 @@ massPic2.addEventListener("mouseout", setOldImage2);
 const massPic3= document.getElementById("massPic3")
 
 function setNewImage3(){
-    massPic3.src ="MassPic.jpeg";
+    massPic3.src ="img/ida2.png";
 }
 
 function setOldImage3(){
-    massPic3.src = "MassPic.png";
+    massPic3.src = "img/ida1.png";
 }
 
 massPic3.addEventListener("mouseover", setNewImage3);
@@ -40,11 +40,11 @@ massPic3.addEventListener("mouseout", setOldImage3);
 const massPic4= document.getElementById("massPic4")
 
 function setNewImage4(){
-    massPic4.src ="MassPic.jpeg";
+    massPic4.src ="img/khai2.png";
 }
 
 function setOldImage4(){
-    massPic4.src = "MassPic.png";
+    massPic4.src = "img/khai1.png";
 }
 
 massPic4.addEventListener("mouseover", setNewImage4);
