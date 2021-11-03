56ebf9a32d43c44291b7626e294f1581e3209678
KhaiLit <khai.sigve@gmail.com>
2021-11-03 18:46:38 +0100

    endra noe greier

--- script/navbar_footer.js
+++ script/navbar_footer.js
@@ -3,7 +3,7 @@ const footer = document.getElementById('footer')
 
 header.innerHTML=`
 <div class="navbar">
-    <!--<a href="index.html" class="navbarLogo"><img src="img/LogoKoi.png" alt="KOI Massasje"></a>-->
+    <<a href="index.html" class="navbarLogo"><img src="img/LogoKoi-invers.png" alt="KOI Massasje"></a>
     <div class='navBarLinks'>
         <a href="index.html#ourTreatments" class="navbarLink">What we offer</a>
         <a href="about_us.html" class="navbarLink">About us</a>
